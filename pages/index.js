7a0ec67dde84017c6b889b8c42b1654ab1e2f6db
etesser <edsontesser@hotmail.com>
2021-05-28 09:02:55 -0300

    teste2

--- pages/index.js
+++ pages/index.js
@@ -13,12 +13,11 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Gerador de etiquetas!
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Selecione uma opção de layout:
         </p>
 
         <div className={styles.grid}>
